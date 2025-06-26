4977aa993bca91774baed0f04d0a7b38991d3d99
RUBENSNETO0001 <rubensnetomartinssuarezneto@gmail.com>
2025-06-25 23:50:08 -0300

    fiz a conecção base de uma mensagem para outra

--- assent/script/scriptConfirmacao.js
+++ assent/script/scriptConfirmacao.js
@@ -1,1 +1,17 @@
-document.addEventListener("click",)
+function confirmacao(event){
+    event.preventDefault();
+    let random = Math.floor(Math.random() * 3);
+    let nome = document.getElementById("input_nome").value;
+
+    if(nome === "amor" || nome === "Amor"){
+        document.getElementById('camada01').style.display = 'none';
+        document.getElementById('camada02').style.display = 'block';
+    }
+    else if(nome === ''){alert("Coloque um nome para prosseguir!");}
+    else{
+        if(random === 0){alert("Tem certeza?");}
+        else if(random === 1){alert("Você tem certeza?");}
+        else{alert ("vou da uma dica \"amor\"o kk");}
+    }
+    return false;
+}
